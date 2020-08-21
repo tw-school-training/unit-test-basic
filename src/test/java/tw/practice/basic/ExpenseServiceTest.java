1280b7a0cd6a705dc5d66ed8a667bb1d61c37129
Zhaoyu <weixiaobo88@gmail.com>
2020-08-21 15:06:21 +0800

    update test case name should_return_internal_expense_type_when_get_expense_type_from_service_given_project_is_internal

--- src/test/java/tw/practice/basic/ExpenseServiceTest.java
+++ src/test/java/tw/practice/basic/ExpenseServiceTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 public class ExpenseServiceTest {
     @Test
-    public void should_return_internal_expense_type_if_project_is_internal() {
+    public void should_return_internal_expense_type_when_get_expense_type_from_service_given_project_is_internal() {
         // given
 
         // when
